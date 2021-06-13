cf76c6f45aae482febd8219e47edb943ce679011
NowNewNao <naoyosetani@gmail.com>
2021-06-13 11:18:29 +0900

    SET router, apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import './App.module.css';
+import styles from './App.module.css';
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Auth from "./components/Auth";
+import MainPage from "./components/MainPage";
+
+const client = new ApolloClient({
+  uri: "ttp://127.0.0.1:8000/graphql/",
+  headers: {
+    authorization: localStorage.getItem("token")
+      ? `JWT ${localStorage.getItem("token")}`
+      : "",
+  },
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ApolloProvider client={client}>
+      <div className={styles.app}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Auth}/>
+            <Route exact path="employees" component={MainPage}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </ApolloProvider>
   );
 }
 
